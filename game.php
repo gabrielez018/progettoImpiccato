39aad8e218a7cb17c7182a0a53b3b627080fcf53
thomas-fragrante <thomas.fragrante@allievi.bearzi.it>
2026-02-12 17:26:37 +0100

    Update game.php
    aggiunto delle sessioni per la logica per indovinare le lettere + logica principale dell' impiccato

--- game.php
+++ game.php
@@ -18,4 +18,33 @@ if (!isset($_SESSION['words']) || empty($_SESSION['words'])) {
     }
     $_SESSION['words'] = $words;
 }
+    if (isset($_POST['new_game'])) {
+    $_SESSION['selectedWord'] = $_SESSION['words'][array_rand($_SESSION['words'])];
+    $_SESSION['displayWord'] = str_repeat('_ ', strlen($_SESSION['selectedWord']));
+    $_SESSION['attemptsLeft'] = 6;
+    $_SESSION['guessedLetters'] = '';
+}
+
+if (isset($_POST['letter']) && !isset($_POST['new_game'])) {
+    $letter = strtoupper(trim($_POST['letter'])[0]);
+    if (strlen($letter) === 1 && ctype_alpha($letter) && strpos($_SESSION['guessedLetters'], $letter) === false) {
+        $selectedWord = $_SESSION['selectedWord'];
+        $wordLength = strlen($selectedWord);
+        $displayArray = str_split($_SESSION['displayWord']);
+        
+        if (strpos($selectedWord, $letter) !== false) {
+            for ($j = 0; $j < $wordLength; $j++) {
+                if ($selectedWord[$j] === $letter) {
+                    $displayArray[2 * $j] = $letter;
+                }
+            }
+            $_SESSION['displayWord'] = implode('', $displayArray);
+        } else {
+            $_SESSION['attemptsLeft']--;
+        }
+        $_SESSION['guessedLetters'] .= $letter;
+    }
+}
+
 ?>
+
