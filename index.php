3546dbcaf32a32333cdcd2a2c94e471036fcaf0d
gabrielez018 <gabrielezampis1807@gmail.com>
2026-02-13 23:42:47 +0100

    update

--- index.php
+++ index.php
@@ -13,37 +13,51 @@
     <title>impiccato</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alexandria:wght@100..900&display=swap" rel="stylesheet">
+
+
 </head>
     <body>
-        <div>
-            <h1>gioco dell'impiccato </h1>
-            <h2>livelli disponibili</h2>
-        </div>
-        <div class="levels">
-        <form action="game.php" method="POST">
-            <?php
+        <div class="content">
+            <div>
+                <h1>gioco dell'impiccato </h1>
+                <h2>livelli disponibili</h2>
+            </div>
+            <div class="levels">
+            <form action="game.php" method="POST">
+                <?php
 
-                require_once('tools.php');
+                    require_once('tools.php');
 
-                $levels = showLevel();
+                    $levels = showLevel();
 
-                if (!empty($levels)) {
-                    echo "<div class='list-group mb-3'>"; 
-                    
-                    foreach ($levels as $index => $livello) {
-                        echo "<div class='form-check'>";
+                    if (!empty($levels)) {
+                        echo "<div class='list-group mb-3'>"; 
                         
-                        echo "  <input class='form-check-input' type='radio' name='level' id='lvl_$index' value='$livello' required>";
-                        echo "  <label class='form-check-label' for='lvl_$index'> Livello $livello </label>";
-                        
+                        foreach ($levels as $index => $livello) {
+                            echo "<div class='form-check'>";
+                            
+                            echo "  <input class='form-check-input' type='radio' name='level' id='lvl_$index' value='$livello' required>";
+                            echo "  <label class='form-check-label' for='lvl_$index'> Livello $livello </label>";
+                            
+                            echo "</div>";
+                        }
                         echo "</div>";
+                        echo "<button type='submit' class='btn btn-primary'>Inizia a giocare!</button>";
                     }
-                    echo "</div>";
-                    echo "<button type='submit' class='btn btn-primary'>Inizia a giocare!</button>";
-                }
-            ?>
-        </form>
-    </div>
+                ?>
+            </form>
+        </div>
+        </div>
+        
     
-</body>
+    </body>
 </html>
