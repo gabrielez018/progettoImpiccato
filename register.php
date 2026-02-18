1140cb404c3f74908ef8462fa45df2bdb88e5dce
gabrielez018 <gabrielezampis1807@gmail.com>
2026-02-18 17:57:20 +0100

    Update register.php

--- register.php
+++ register.php
@@ -2,9 +2,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>registrazooione</title>
 </head>
 <body>
-    
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
