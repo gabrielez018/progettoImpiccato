138b58d5f88a2bfc0eb85341df9cf87c58334075
gabrielez018 <gabrielezampis1807@gmail.com>
2026-02-18 18:08:49 +0100

    update e rinominazione

---
+++ register-save.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+include 'db.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+    
+}
+?>
