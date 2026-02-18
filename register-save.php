aa1783843d450639d17bb293a5e8d19166c0d50c
gabrielez018 <gabrielezampis1807@gmail.com>
2026-02-18 18:13:32 +0100

    Update register-save.php

--- register-save.php
+++ register-save.php
@@ -4,6 +4,18 @@ include 'db.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'] ?? '';
     $password = $_POST['password'] ?? '';
-    
+
+    if (!empty($username) && !empty($password)) {
+        if(saveUser($username,$password)){
+            $_SESSION['username'] = $username;
+            header('Location: index.php');
+            exit;
+        }
+        else{
+            echo "utente gia esistente";
+            header(Location: 'register.php');
+        }
+    }
 }
+header('Location: register.php');
 ?>
